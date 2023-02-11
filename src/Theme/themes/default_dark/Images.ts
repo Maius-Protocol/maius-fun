f0d7cfca1dc5120390746d312f17838266e1fd0d
Thomas Hoang <megatunger@gmail.com>
2023-02-11 23:19:06 +0700

    Reinit Project

---
+++ src/Theme/themes/default_dark/Images.ts
@@ -0,0 +1,11 @@
+/**
+ * Images should be stored in the `App/Images` directory and referenced using variables defined here.
+ */
+
+export default function () {
+  return {
+    logo: {
+      uri: 'https://thecodingmachine.github.io/react-native-mairdrop/img/TOM-small.png',
+    },
+  }
+}
