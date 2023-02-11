7237c028d344a07e84b264b8d7381fec41955ed5
Thomas Hoang <megatunger@gmail.com>
2023-02-12 01:12:55 +0700

    Link brand fonts into app

---
+++ FONTS-README.md
@@ -0,0 +1,10 @@
+# FONTS-README
+
+Now you can use the following fonts:
+
+"- `Stylesheet.create({ someText: { fontFamily: "DMSans-Regular" }});`
+"- `Stylesheet.create({ someText: { fontFamily: "DMSans-Italic" }});`
+"- `Stylesheet.create({ someText: { fontFamily: "DMSans-Medium" }});`
+"- `Stylesheet.create({ someText: { fontFamily: "DMSans-MediumItalic" }});`
+"- `Stylesheet.create({ someText: { fontFamily: "DMSans-Bold" }});`
+"- `Stylesheet.create({ someText: { fontFamily: "DMSans-BoldItalic" }});`
--- src/Theme/Fonts.ts
+++ src/Theme/Fonts.ts
@@ -3,9 +3,14 @@
  */
 import { StyleSheet } from 'react-native'
 import { ThemeVariables } from './theme'
+import { Colors } from '@/Theme/Variables'
 
 export default function ({ FontSize, Colors }: ThemeVariables) {
   return StyleSheet.create({
+    regular: { fontFamily: 'DMSans-Regular' },
+    italic: { fontFamily: 'DMSans-Italic' },
+    medium: { fontFamily: 'DMSans-Medium' },
+    bold: { fontFamily: 'DMSans-Bold' },
     textSmall: {
       fontSize: FontSize.small,
       color: Colors.text,
@@ -44,6 +49,15 @@ export default function ({ FontSize, Colors }: ThemeVariables) {
     },
     textRight: {
       textAlign: 'right',
+    },
+    textGray: {
+      color: Colors.text,
+    },
+    textBlack: {
+      color: 'black',
+    },
+    textWhite: {
+      color: 'white',
     },
   })
 }
