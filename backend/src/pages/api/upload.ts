4152bae18e4d39a9c1eeb8c3c1ccd5697e86ab6f
Huy Nguyen Quang <39359294+quanghuynguyen1902@users.noreply.github.com>
2023-02-16 01:31:13 +0700

    feat: upload file to s3

--- backend/src/pages/api/upload.ts
+++ backend/src/pages/api/upload.ts
@@ -1,25 +1,97 @@
 import S3 from 'aws-sdk/clients/s3'
 import { NextApiRequest, NextApiResponse } from 'next'
+import formidable from 'formidable'
+import { v4 } from "uuid";
+import * as fs from "fs";
+
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
+    req: NextApiRequest,
+    res: NextApiResponse,
 ) {
+  const accessKeyId = process.env.ACCESS_KEY
+  const secretAccessKey = process.env.SECRET_KEY
+  const bucket = process.env.BUCKET
+  const region = process.env.REGION
+  const folderS3 = process.env.FOLDER_S3
+
   const s3 = new S3({
-    apiVersion: '2006-03-01',
+    region: region,
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey,
+    signatureVersion: "v4",
   })
 
-  const post = await s3.createPresignedPost({
-    Bucket: process.env.BUCKET_NAME,
-    Fields: {
-      key: req.query.file,
-      'Content-Type': req.query.fileType,
-    },
-    Expires: 60, // seconds
-    Conditions: [
-      ['content-length-range', 0, 1048576], // up to 1 MB
-    ],
-  })
+  const {errorParseFile, fields, files } = await parseForm(req);
+  if (errorParseFile) {
+    return res.status(400).json({"error": errorParseFile})
+  }
 
-  res.status(200).json(post)
+  let fileStream = fs.createReadStream(files.filename.filepath);
+  let fileId = v4()
+  let key = folderS3 + "/" + fileId
+  let type = files.filename.mimetype
+
+  const params = {
+    Bucket: bucket,
+    Key: key,
+    Body: fileStream,
+    ContentType: type,
+    Expires: 600
+  }
+
+  await s3.upload(params, async function(err, res1) {
+    if (err) {
+      return res.status(400).json({"err": err})
+    } else {
+      let url = "https://" + bucket + "." + "s3-" + region + ".amazonaws.com/" + key
+      let nameJson = v4()
+      let keyJson = folderS3 + "/" + nameJson + '.json'
+      let type = "application/json"
+      let data = {
+        name: fields.name,
+        image: url
+      }
+      const paramsJson = {
+        Bucket: bucket,
+        Key: keyJson,
+        Body: JSON.stringify(data),
+        ContentType: type,
+        Expires: 600
+      }
+
+      await s3.upload(paramsJson, async function(err, res2) {
+        if (err) {
+          return res.status(400).json({"err": err})
+        } else {
+          let urlJson = "https://" + bucket + "." + "s3-" + region + ".amazonaws.com/" + keyJson
+          return res.status(200).json({"url": urlJson})
+        }
+      })
+    }
+  });
+
 }
+
+export const parseForm = async (
+    req: NextApiRequest
+): Promise<{errorParseFile: Error, fields: formidable.Fields; files: formidable.Files }> => {
+  return new Promise(async (resolve, reject) => {
+    const form = formidable({
+      maxFiles: 2,
+      maxFileSize: 1024 * 1024, // 1mb
+    });
+    form.parse(req, function (errorParseFile, fields, files) {
+      if (errorParseFile) resolve({errorParseFile, fields, files});
+      else resolve({errorParseFile, fields, files });
+    });
+  });
+};
+
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
